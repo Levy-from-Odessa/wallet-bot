5a4d6764af0495971dcfbdc508d68896f46456b9
saveliy <savelii.moshkota@ext.jumingo.com>
2023-02-01 21:28:58 +0200

    add income page

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,5 +1,5 @@
 import  axios from 'axios'
-import { env } from '$env/dynamic/private';
+import { env } from '$env/dynamic/private'; //here
 const Api = axios.create({
   baseURL: env.WALLET_API,
   timeout: 30000
