1ac295094a72b28fd1c86f09cf610ebd75057f11
saveliy <savelii.moshkota@ext.jumingo.com>
2023-10-17 10:49:17 -0700

    fix: validateToken middleware logic

--- server/src/middlewares/validateToken.js
+++ server/src/middlewares/validateToken.js
@@ -7,11 +7,11 @@ const {Wallet} = require('../models')
 module.exports = async (req, res, next) => {
   //get token from request header
   const token = req.headers["authorization"]?.split(" ")[1] || null
-  if (!token) res.status(400).send("Token not present")
+  if (!token) return res.status(400).send("Token not present")
 
   await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, name) => {
     if (err) {
-      res.status(403).send("Token invalid")
+      return res.status(403).send("Token invalid")
     }
     else {
       const user = (await Wallet.findOne({where: {name: name.user}}))
